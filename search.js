0a10b5c075608ab09db977791ec80e1d923b24af
ShubhangiMalik <55026486+ShubhangiMalik@users.noreply.github.com>
2020-06-25 01:25:26 +0530

    JS File

---
+++ search.js
@@ -0,0 +1,46 @@
+var movie=document.querySelectorAll(".movie")
+var year=document.querySelectorAll(".year")
+var type=document.querySelectorAll(".type")
+var image=document.querySelectorAll(".image")
+var content=document.querySelector("#content")
+var submitBtn = document.querySelector("#submit")
+var inputBox=document.querySelector("#userInput")
+var userInput =inputBox.value
+var buttons=document.querySelector("#buttons")
+var again=document.querySelector("#again")
+
+
+
+function fetchingData(userInput) {
+    var userInput =document.querySelector("#userInput").value
+    fetch('http://www.omdbapi.com/?apikey=56d64f03&s=' + userInput)
+    .then((response) => { 
+        response.json().then((data) => {
+        for(var i=0;i<10;i++){
+            movie[i].textContent = data.Search[i].Title
+            image[i].src=data.Search[i].Poster
+            year[i].textContent = data.Search[i].Year
+            type[i].textContent = data.Search[i].Type
+        }
+        
+           
+        }).catch((err) => {
+            console.log(err);
+        })
+    });
+}
+
+submitBtn.addEventListener("click", function(){
+    fetchingData()
+    content.classList.remove("hide")
+    buttons.classList.add("hide")
+    again.classList.remove("hide")
+
+})
+
+again.addEventListener("click",function(){
+    buttons.classList.remove("hide")
+    again.classList.add("hide")
+    content.classList.add("hide")
+    inputBox.value=""
+})
